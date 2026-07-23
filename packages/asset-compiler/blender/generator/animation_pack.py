27ed212582dacb59f21b63ae6a38f06cb6c783de
dev.cunningham <dev.jon.cunningham@gmail.com>
2026-07-23 18:55:02 -0500

    Preserve cumulative animation pose channels

--- packages/asset-compiler/blender/generator/animation_pack.py
+++ packages/asset-compiler/blender/generator/animation_pack.py
@@ -67,11 +67,11 @@ CLIPS = (
         True,
         False,
         (
-            KeyPose(1, {"thigh.L": (radians(24), 0.0, 0.0), "thigh.R": (radians(-24), 0.0, 0.0), "upper_arm.L": (radians(-16), 0.0, 0.0), "upper_arm.R": (radians(16), 0.0, 0.0)}),
+            KeyPose(1, {"thigh.L": (radians(24), 0.0, 0.0), "thigh.R": (radians(-24), 0.0, 0.0), "shin.L": (0.0, 0.0, 0.0), "shin.R": (0.0, 0.0, 0.0), "upper_arm.L": (radians(-16), 0.0, 0.0), "upper_arm.R": (radians(16), 0.0, 0.0)}),
             KeyPose(7, {"thigh.L": (0.0, 0.0, 0.0), "thigh.R": (0.0, 0.0, 0.0), "shin.L": (radians(18), 0.0, 0.0), "shin.R": (radians(6), 0.0, 0.0)}),
-            KeyPose(13, {"thigh.L": (radians(-24), 0.0, 0.0), "thigh.R": (radians(24), 0.0, 0.0), "upper_arm.L": (radians(16), 0.0, 0.0), "upper_arm.R": (radians(-16), 0.0, 0.0)}),
+            KeyPose(13, {"thigh.L": (radians(-24), 0.0, 0.0), "thigh.R": (radians(24), 0.0, 0.0), "shin.L": (0.0, 0.0, 0.0), "shin.R": (0.0, 0.0, 0.0), "upper_arm.L": (radians(16), 0.0, 0.0), "upper_arm.R": (radians(-16), 0.0, 0.0)}),
             KeyPose(19, {"thigh.L": (0.0, 0.0, 0.0), "thigh.R": (0.0, 0.0, 0.0), "shin.L": (radians(6), 0.0, 0.0), "shin.R": (radians(18), 0.0, 0.0)}),
-            KeyPose(25, {"thigh.L": (radians(24), 0.0, 0.0), "thigh.R": (radians(-24), 0.0, 0.0), "upper_arm.L": (radians(-16), 0.0, 0.0), "upper_arm.R": (radians(16), 0.0, 0.0)}),
+            KeyPose(25, {"thigh.L": (radians(24), 0.0, 0.0), "thigh.R": (radians(-24), 0.0, 0.0), "shin.L": (0.0, 0.0, 0.0), "shin.R": (0.0, 0.0, 0.0), "upper_arm.L": (radians(-16), 0.0, 0.0), "upper_arm.R": (radians(16), 0.0, 0.0)}),
         ),
     ),
     ClipSpec(
@@ -81,7 +81,7 @@ CLIPS = (
         False,
         False,
         (
-            KeyPose(1, {"shoulder.R": (0.0, 0.0, radians(12)), "upper_arm.R": (0.0, 0.0, radians(55)), "forearm.R": (radians(-80), 0.0, 0.0)}),
+            KeyPose(1, {"shoulder.R": (0.0, 0.0, radians(12)), "upper_arm.R": (0.0, 0.0, radians(55)), "forearm.R": (radians(-80), 0.0, 0.0), "hand.R": (0.0, radians(12), 0.0)}),
             KeyPose(13, {"hand.R": (0.0, radians(-24), 0.0)}),
             KeyPose(25, {"hand.R": (0.0, radians(24), 0.0)}),
             KeyPose(37, {"hand.R": (0.0, radians(-24), 0.0)}),
@@ -159,16 +159,14 @@ def apply_animation_pack(armature) -> dict:
         action["fps"] = FPS
         armature.animation_data.action = action
 
+        current_rotations = {bone.name: (0.0, 0.0, 0.0) for bone in armature.pose.bones}
         for bone in armature.pose.bones:
             bone.rotation_mode = "XYZ"
-            bone.rotation_euler = (0.0, 0.0, 0.0)
+            bone.rotation_euler = current_rotations[bone.name]
         for pose in clip.key_poses:
+            current_rotations.update(pose.rotations)
             for bone in armature.pose.bones:
-                bone.rotation_euler = (0.0, 0.0, 0.0)
-            for bone_name, rotation in pose.rotations.items():
-                bone = armature.pose.bones[bone_name]
-                bone.rotation_euler = rotation
-            for bone in armature.pose.bones:
+                bone.rotation_euler = current_rotations[bone.name]
                 bone.keyframe_insert("rotation_euler", frame=pose.frame, group=bone.name)
         for curve in action.fcurves:
             for point in curve.keyframe_points:
